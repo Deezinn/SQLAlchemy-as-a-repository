1b8bcb3c862664d6fb6545cc98d5b3ba70861fe3
Deezinn <developer.andrelz@gmail.com>
2026-04-06 22:30:04 -0300

    feat: simple select

--- estudo/use_case/working_with_data/select.py
+++ estudo/use_case/working_with_data/select.py
@@ -1,1 +1,20 @@
-""
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from estudo.connection import engine
+from estudo.use_case.working_with_database_metadata.with_declarative_base import User
+
+stmt = select(User).where(User.name == "spongebob")
+
+print(stmt)
+
+
+with engine.connect() as conn:
+    for row in conn.execute(stmt):
+        print(row)
+
+with Session(engine) as session:
+    for row in session.execute(stmt):
+        print(row)
+
+
