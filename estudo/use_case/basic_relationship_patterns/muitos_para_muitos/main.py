f34eedab23a3e1c7a47a0b814cbd37e3696e53cc
Deezinn <developer.andrelz@gmail.com>
2026-04-16 17:33:46 -0300

    feat: create table example

---
+++ estudo/use_case/basic_relationship_patterns/muitos_para_muitos/main.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from sqlalchemy import Column
+from sqlalchemy import Table
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import relationship
+
+class Base(DeclarativeBase):
+    pass
+
+association_table = Table(
+    "association_table",
+    Base.metadata,
+    Column("left_id", ForeignKey("left_table.id")),
+    Column("right_id", ForeignKey("right_table.id"))
+)
+
+class Parent(Base):
+    __tablename__ = "left_table"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    children: Mapped[list[Child]] = relationship(secondary=association_table)
+
+class Child(Base):
+    __tablename__ = "right_table"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
