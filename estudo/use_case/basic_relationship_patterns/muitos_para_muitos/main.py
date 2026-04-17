b2e8dbfabd19f960450e3111838e2cbd3cb8ec97
Deezinn <developer.andrelz@gmail.com>
2026-04-16 22:03:12 -0300

    feat: create table many-to-many

--- estudo/use_case/basic_relationship_patterns/muitos_para_muitos/main.py
+++ estudo/use_case/basic_relationship_patterns/muitos_para_muitos/main.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+from typing import Optional
+from typing import List
+
 from sqlalchemy import Column
 from sqlalchemy import Table
 from sqlalchemy import ForeignKey
@@ -9,23 +12,48 @@ from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import relationship
 
+# class Base(DeclarativeBase):
+#     pass
+
+# association_table = Table(
+#     "association_table",
+#     Base.metadata,
+#     Column("left_id", ForeignKey("left_table.id"), primary_key=True),
+#     Column("right_id", ForeignKey("right_table.id"), primary_key=True)
+# )
+
+# class Parent(Base):
+#     __tablename__ = "left_table"
+
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     children: Mapped[list[Child]] = relationship(back_populates="parents",secondary=association_table)
+
+# class Child(Base):
+#     __tablename__ = "right_table"
+
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     parents: Mapped[list[Parent]] = relationship(back_populates="children", secondary=association_table)
+
 class Base(DeclarativeBase):
     pass
 
-association_table = Table(
-    "association_table",
-    Base.metadata,
-    Column("left_id", ForeignKey("left_table.id")),
-    Column("right_id", ForeignKey("right_table.id"))
-)
+
+class Association(Base):
+    __tablename__ = "association_table"
+    left_id: Mapped[int] = mapped_column(ForeignKey("left_table.id"), primary_key=True)
+    right_id: Mapped[int] = mapped_column(
+        ForeignKey("right_table.id"), primary_key=True
+    )
+    extra_data: Mapped[Optional[str]]
+    child: Mapped["Child"] = relationship()
+
 
 class Parent(Base):
     __tablename__ = "left_table"
-
     id: Mapped[int] = mapped_column(primary_key=True)
-    children: Mapped[list[Child]] = relationship(secondary=association_table)
+    children: Mapped[List["Association"]] = relationship()
+
 
 class Child(Base):
     __tablename__ = "right_table"
-
     id: Mapped[int] = mapped_column(primary_key=True)
